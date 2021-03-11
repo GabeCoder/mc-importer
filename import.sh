bc5018efe6310e7b1f9bb86bb14d025913ed7046
GabeTheCoder <72320038+GabeCoder@users.noreply.github.com>
2021-03-11 18:43:52 -0500

    Update import.sh

--- import.sh
+++ import.sh
@@ -4,26 +4,27 @@ then
 	then
 		basename=$(basename -a $archive)
 		yad --list --title="Minecraft Archive Guide" --text="Here is a list of archive types to help you." --column="Minecraft Archive" --column="Archive Extension" World .mcworld Addon .mcaddon "Resource/Behavior Pack" .mcpack --no-buttons &
-		yad --title="Archive Type" --text="What type of archive are you importing?" --button="World":0 --button="Addon":1 --button="Resource Pack":2 --button="Behavior Pack":3 --width=300 --height=150
-		if [ $? -eq 0 ]
+		yad --title="Archive Type" --text="What type of archive are you importing?" --button="World":10 --button="Addon":20 --button="Resource Pack":30 --button="Behavior Pack":40 --width=300 --height=150
+		answer=$?
+		if [ $answer -eq 10 ]
 		then
 			cd $HOME/.local/share/mcpelauncher/games/com.mojang/minecraftWorlds
 			mkdir "$basename"
 			unzip "$archive" -d "$basename"
 			zenity --info --icon-name=emblem-default --title="World Imported" --text="Successfully imported $basename." --width=300
 		else
-			if [ $? -eq 1 ]
+			if [ $answer -eq 20 ]
 			then
 				zenity --info --title="Import Addon" --text="To import an addon, you'll need to extract the packs from it to import here."
 			else
-				if [ $? -eq 2 ]
+				if [ $answer -eq 30 ]
 				then
 					cd $HOME/.local/share/mcpelauncher/games/com.mojang/resource_packs
 					mkdir "$basename"
 					unzip "$archive" -d "$basename"
 					zenity --info --icon-name=emblem-default --title="Pack Imported" --text="Successfully imported $basename." --width=300
 				else
-					if [ $? -eq 3 ]
+					if [ $answer -eq 40 ]
 					then
 						cd $HOME/.local/share/mcpelauncher/games/com.mojang/behavior_packs
 						mkdir "$basename"
