e887a951bba32cdba09469d39addd99276623f9a
GabeTheCoder <72320038+GabeCoder@users.noreply.github.com>
2021-04-26 19:53:28 -0400

    Removed Introduction and Addon Import
    The intro and addon importing features were removed due to time-consuming issues and hard-to-code frustration.

--- import.sh
+++ import.sh
@@ -1,40 +1,30 @@
-if yad --window-icon="$HOME"/mc-importer/icon.png --title="Minecraft Importer" --text="Welcome to the Minecraft Importer! To continue, click OK. Otherwise, click Cancel." --width=300 --height=150
+#!/bin/bash
+
+cd "$HOME"/mc-importer
+if archive=$(zenity --file-selection --title="Select Package")
 then
-	if archive=$(zenity --file-selection --title="Archive Selection")
+	basename=$(basename -a $archive)
+	yad --window-icon=package --image=icon.png --title="Package Type" --text="What package are you importing?" --button="World":10 --button="Resource Pack":20 --button="Behavior Pack":30 --width=300 --height=100
+	answer=$?
+	name=$(zenity --entry --title="Package Name" --text="What do you want to name the contents folder?" --entry-text="$basename")
+	if [ $answer == 10 ]
 	then
-		basename=$(basename -a $archive)
-		yad --list --window-icon=package --title="Minecraft Archive Guide" --text="Here is a list of archive types to help you." --column="Minecraft Archive" --column="Archive Extension" World .mcworld Addon .mcaddon "Resource/Behavior Pack" .mcpack --no-buttons &
-		yad --window-icon=package --title="Archive Type" --text="What type of archive are you importing?" --button="World":10 --button="Addon":20 --button="Resource Pack":30 --button="Behavior Pack":40 --width=300 --height=150
-		answer=$?
-		name=$(zenity --entry --title="Name Archive" --text="What do you want to name the folder of the archive?" --entry-text="$basename")
-		if [ $answer -eq 10 ]
-		then
-			cd "$HOME"/.local/share/mcpelauncher/games/com.mojang/minecraftWorlds
-			mkdir "$name"
-			unzip "$archive" -d "$name"
-			zenity --info --icon-name=emblem-default --title="World Imported" --text="Successfully imported $basename." --width=300
-		else
-			if [ $answer -eq 20 ]
-			then
-				zenity --info --title="Import Addon" --text="To import an addon, you'll need to extract the packs from it to import here."
-			else
-				if [ $answer -eq 30 ]
-				then
-					cd "$HOME"/.local/share/mcpelauncher/games/com.mojang/resource_packs
-					mkdir "$name"
-					unzip "$archive" -d "$name"
-					zenity --info --icon-name=emblem-default --title="Pack Imported" --text="Successfully imported $basename." --width=300
-				else
-					if [ $answer -eq 40 ]
-					then
-						cd "$HOME"/.local/share/mcpelauncher/games/com.mojang/behavior_packs
-						mkdir "$name"
-						unzip "$archive" -d "$name"
-						zenity --info --icon-name=emblem-default --title="Pack Imported" --text="Successfully imported $basename." --width=300
-					fi
-				fi
-			fi
-		fi
-		zenity --info --title="Important" --text="Restart Minecraft for the imported files to be applied to the game." --width=300
+		cd "$HOME"/.local/share/mcpelauncher/games/com.mojang/minecraftWorlds
+		mkdir "$name"
+		unzip "$archive" -d "$name"
+		zenity --info --icon-name=emblem-default --title="World Imported" --text="Successfully imported $basename." --width=300
+	elseif [ $answer == 20 ]
+	then
+		cd "$HOME"/.local/share/mcpelauncher/games/com.mojang/resource_packs
+		mkdir "$name"
+		unzip "$archive" -d "$name"
+		zenity --info --icon-name=emblem-default --title="Pack Imported" --text="Successfully imported $basename." --width=300
+	elseif [ $answer == 30 ]
+	then
+		cd "$HOME"/.local/share/mcpelauncher/games/com.mojang/behavior_packs
+		mkdir "$name"
+		unzip "$archive" -d "$name"
+		zenity --info --icon-name=emblem-default --title="Pack Imported" --text="Successfully imported $basename." --width=300
 	fi
+	zenity --info --title="Restart" --text="Restart Minecraft to complete the import." --width=300
 fi
