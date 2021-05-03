e2ed6a2ba66f78a998c811c5d9b9fdb0d7f2ce76
GabeTheCoder <72320038+GabeCoder@users.noreply.github.com>
2021-05-03 19:00:40 -0400

    Fixed naming system

--- import.sh
+++ import.sh
@@ -3,7 +3,7 @@
 cd "$HOME"/mc-importer
 if archive=$(zenity --file-selection --title="Select Package")
 then
-	basename=$(basename -a $archive)
+	basename=$(basename -a "$archive")
 	yad --window-icon=package --image=icon.png --title="Package Type" --text="What package are you importing?" --button="World":10 --button="Resource Pack":20 --button="Behavior Pack":30 --width=300 --height=100
 	answer=$?
 	name=$(zenity --entry --title="Package Name" --text="What do you want to name the contents folder?" --entry-text="$basename")
