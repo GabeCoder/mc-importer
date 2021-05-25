9c32d1cb7ded12f9d2a5e4cc139dc145bfb1b385
GabeTheCoder <72320038+GabeCoder@users.noreply.github.com>
2021-05-25 01:12:03 -0400

    New World Template Option
    A new feature has been added to Minecraft Importer which allows world templates to be imported.

--- import.sh
+++ import.sh
@@ -4,7 +4,7 @@ cd "$HOME"/mc-importer
 if archive=$(zenity --file-selection --title="Select Package")
 then
 	basename=$(basename -a "$archive")
-	yad --window-icon=package --image=icon.png --title="Package Type" --text="What package are you importing?" --button="World":10 --button="Resource Pack":20 --button="Behavior Pack":30 --width=300 --height=100
+	yad --window-icon=package --image=icon.png --title="Package Type" --text="What package are you importing?" --button="World":10 --button="World Template":20 --button="Resource Pack":30 --button="Behavior Pack":40 --width=300 --height=100
 	answer=$?
 	name=$(zenity --entry --title="Package Name" --text="What do you want to name the contents folder?" --entry-text="$basename")
 	if [ $answer == 10 ]
@@ -15,16 +15,22 @@ then
 		zenity --info --icon-name=emblem-default --title="World Imported" --text="Successfully imported $basename." --width=300
 	elif [ $answer == 20 ]
 	then
+		cd "$HOME"/.local/share/mcpelauncher/games/com.mojang/world_templates
+		mkdir "$name"
+		unzip "$archive" -d "$name"
+		zenity --info --icon-name=emblem-default --title="World Template Imported" --text="Successfully imported $basename." --width=300
+	elif [ $answer == 30 ]
+	then
 		cd "$HOME"/.local/share/mcpelauncher/games/com.mojang/resource_packs
 		mkdir "$name"
 		unzip "$archive" -d "$name"
-		zenity --info --icon-name=emblem-default --title="Pack Imported" --text="Successfully imported $basename." --width=300
-	elif [ $answer == 30 ]
+		zenity --info --icon-name=emblem-default --title="Resource Pack Imported" --text="Successfully imported $basename." --width=300
+	elif [ $answer == 40 ]
 	then
 		cd "$HOME"/.local/share/mcpelauncher/games/com.mojang/behavior_packs
 		mkdir "$name"
 		unzip "$archive" -d "$name"
-		zenity --info --icon-name=emblem-default --title="Pack Imported" --text="Successfully imported $basename." --width=300
+		zenity --info --icon-name=emblem-default --title="Behavior Pack Imported" --text="Successfully imported $basename." --width=300
 	fi
 	zenity --info --title="Restart" --text="Restart Minecraft to complete the import." --width=300
 fi
